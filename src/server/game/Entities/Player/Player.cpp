59af47c2a9d67ec9bfc6f214d7a67a6a4b89f595
MrZomble <lukemedson@gmail.com>
2011-10-12 22:46:58 +1000

    waefaw

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -1756,11 +1756,12 @@ void Player::Update(uint32 p_time)
         m_Played_time[PLAYED_TIME_LEVEL] += elapsed;        // Level played time
         m_Last_tick = now;
 		
-		if (m_Played_time[PLAYED_TIME_TOTAL] < 10 * MINUTE * IN_MILLISECONDS)
-		{ 
-		SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
-		}
     }
+	
+	if (m_Played_time[PLAYED_TIME_TOTAL] < 10 * MINUTE * IN_MILLISECONDS)
+	{ 
+		SetFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
+	}
 
     if (m_drunk)
     {
