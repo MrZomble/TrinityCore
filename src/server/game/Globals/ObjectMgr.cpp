5435676dddd471a900921a354a2f9be88b4cf7a5
MrZomble <lukemedson@gmail.com>
2011-10-12 22:26:28 +1000

    trying this method

--- src/server/game/Globals/ObjectMgr.cpp
+++ src/server/game/Globals/ObjectMgr.cpp
@@ -5768,15 +5768,26 @@ WorldSafeLocsEntry const* ObjectMgr::GetClosestGraveYard(float x, float y, float
             return GetDefaultGraveYard(team);
         }
     }
-	AreaTableEntry const* zone = GetAreaEntryByAreaID(zoneID);	
-	if (zone->ID == 267)
-	{
-		if (team == HORDE)
-        return sWorldSafeLocsStore.LookupEntry(98);
-    else if (team == ALLIANCE)
-        return sWorldSafeLocsStore.LookupEntry(149);
-	}	
+	
+ do
+    {
+        ++count;
 
+        Field* fields = result->Fetch();
+
+        uint32 safeLocId = fields[0].GetUInt32();
+        uint32 zoneId = fields[1].GetUInt32();
+		
+		AreaTableEntry const* zone = GetAreaEntryByAreaID(zoneID);	
+		if (zone->ID == 267)
+		{
+			if (team == HORDE)
+			return sWorldSafeLocsStore.LookupEntry(98);
+		else if (team == ALLIANCE)
+			return sWorldSafeLocsStore.LookupEntry(149);
+		}	
+	} while (result->NextRow());
+	
     // Simulate std. algorithm:
     //   found some graveyard associated to (ghost_zone, ghost_map)
     //
