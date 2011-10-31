318db5aed471de9647b18a7c30a65e619d9ecf56
MrZomble <lukemedson@gmail.com>
2011-11-01 09:06:07 +1000

    merge

--- src/server/scripts/Commands/cs_gm.cpp
+++ src/server/scripts/Commands/cs_gm.cpp
@@ -123,8 +123,8 @@ public:
         bool first = true;
         bool footer = false;
 
-        ACE_GUARD_RETURN(ACE_Thread_Mutex, guard, *HashMapHolder<Player>::GetLock(), true);
-        HashMapHolder<Player>::MapType& m = sObjectAccessor->GetPlayers();
+        TRINITY_READ_GUARD(HashMapHolder<Player>::LockType, *HashMapHolder<Player>::GetLock());
+        HashMapHolder<Player>::MapType const& m = sObjectAccessor->GetPlayers();
         for (HashMapHolder<Player>::MapType::const_iterator itr = m.begin(); itr != m.end(); ++itr)
         {
             AccountTypes itrSec = itr->second->GetSession()->GetSecurity();
